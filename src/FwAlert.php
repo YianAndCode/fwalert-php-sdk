8af1efefdf64bb61c07b9d7136874072a450f1ec
Y!an <i@yian.me>
2021-07-11 23:24:14 +0800

    add Webhook method

--- src/FwAlert.php
+++ src/FwAlert.php
@@ -2,7 +2,13 @@
 
 namespace FwAlert;
 
+use FwAlert\Helpers\Http;
+
 class FwAlert
 {
-    //
+    public function Webhook(string $webhook_url, array $variables)
+    {
+        $http_client = new Http;
+        return $http_client->post($webhook_url, $variables);
+    }
 }
