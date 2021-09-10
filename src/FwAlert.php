acfc30df32b329c7e04c1e12ca709e91d46f5b44
Y!an <i@yian.me>
2021-09-11 00:09:54 +0800

    更新 FwAlert,添加 Channels

--- src/FwAlert.php
+++ src/FwAlert.php
@@ -6,9 +6,35 @@ use FwAlert\Helpers\Http;
 
 class FwAlert
 {
-    public function Webhook(string $webhook_url, array $variables)
+    private $channels = [];
+
+    public function SendAlert(string $webhook_url, array $variables)
     {
         $http_client = new Http;
         return $http_client->post($webhook_url, $variables);
+    }
+
+    public function AddChannel(string $channel, string $webhook_url)
+    {
+        $this->channels[$channel] = $webhook_url;
+    }
+
+    public function GetChannel(string $channel): string
+    {
+        return @$this->channels[$channel];
+    }
+
+    public function RemoveChannel(string $channel)
+    {
+        unset($this->channels[$channel]);
+    }
+
+    public function Send(string $channel, array $variables)
+    {
+        $webhook_url = $this->GetChannel($channel);
+        if ($webhook_url) {
+            return $this->Webhook($webhook_url, $variables);
+        }
+        return false;
     }
 }
